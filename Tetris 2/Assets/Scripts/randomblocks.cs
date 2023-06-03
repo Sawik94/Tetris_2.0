4c45610c2ec333d0088be66230da85d1a54abf25
Sawik94 <jean.jean@interia.pl>
2023-06-03 03:56:04 +0200

    Dodany dzialajacy generator losowych ksztaltow

--- Tetris 2/Assets/Scripts/randomblocks.cs
+++ Tetris 2/Assets/Scripts/randomblocks.cs
@@ -15,11 +15,13 @@ public class randomblocks : MonoBehaviour
         public int _x;
         public int _y;
         public int _exists;
+        public int _tag;
     }
 
 
 
-    void Funkcja()
+
+    void SpawnRandom()
     {
 
         Coord[,] tab = new Coord[5, 5];
@@ -32,24 +34,49 @@ public class randomblocks : MonoBehaviour
                 tab[i, j]._x = 3 + i;
                 tab[i, j]._y = 12 + j;
                 tab[i, j]._exists = 0;
+                tab[i, j]._tag = 0;
             }
 
         }
 
-        int k = Random.Range(5, 10);
-
+        //int k = Random.Range(5, 10);
+        int k = 3;
         int randx;
         int randy;
+        birth(tab[2, 2]._x, tab[2, 2]._y);
         tab[2, 2]._exists = 1;
-       
+        tab[2, 1]._tag = 1;
+        tab[2, 3]._tag = 1;
+        tab[1, 2]._tag = 1; 
+        tab[3, 2]._tag = 1;
 
-
-        for (int l = 0; l < k;l++)
+        for (int l = 0; l < k; l++)
         {
+
             randx = Random.Range(0, 4);
             randy = Random.Range(0, 4);
-            if ((tab[randx, randy - 1]._exists == 1 || tab[randx + 1, randy]._exists == 1 || tab[randx - 1, randy]._exists == 1 || tab[randx, randy + 1]._exists == 1) && tab[randx, randy]._exists == 0)
+            if (tab[randx, randy]._tag == 1 && tab[randx, randy]._exists == 0)
             {
+                tab[randx, randy]._tag = 0;
+              
+                if (randx < 4)
+                {
+                    tab[randx + 1, randy]._tag = 1;
+                }
+                if (randx > 0)
+                {
+                    tab[randx - 1, randy]._tag = 1;
+                }
+
+                if (randy < 4)
+                {
+                    tab[randx, randy+1]._tag = 1;
+                }
+                if (randy > 0)
+                {
+                    tab[randx, randy-1]._tag = 1;
+                }
+
                 birth(tab[randx, randy]._x, tab[randx, randy]._y);
                 tab[randx, randy]._exists = 1;
 
@@ -58,30 +85,8 @@ public class randomblocks : MonoBehaviour
             {
                 l--;
             }
-            birth(tab[2, 2]._x, tab[2, 2]._y);
+
         }
-
-
-        //for (int l = 1; l < k; )
-        //{
-        //    randx = Random.Range(0, 4);
-        //    randy = Random.Range(0, 4);
-
-        //    if (tab[randx, randy]._exists == 0 && (tab[randx, randy - 1]._exists == 1 || tab[randx + 1, randy]._exists == 1 || tab[randx - 1, randy]._exists == 1 || tab[randx, randy+1]._exists == 1))
-        //    {
-        //        tab[randx, randy]._exists = 1;
-        //        birth(tab[randx, randy]._x, tab[randx, randy]._y);
-
-        //        l++;
-        //    }
-        //    else
-        //    {
-        //        continue;
-        //    }
-
-
-
-        //}
 
 
 
@@ -105,11 +110,10 @@ public class randomblocks : MonoBehaviour
     void Start()
     {
 
-        Funkcja();
+        SpawnRandom();
   
     }
 
-    // Update is called once per frame
     void Update()
     {
         
